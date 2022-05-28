e815edff8278d493cefcad028fa15d9394f002df
RyosukeSomeya <rx.0.0618@gmail.com>
2022-05-29 01:27:14 +0900

    自動デプロイ確認用変更

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Layout from '../components/Layout';
 
-const Contact = () => {
+export default function Contact() {
   return (
     <Layout title='Contact'>
       <div className='bg-white text-center shadow-xl p-8 w-80 rounded'>
@@ -80,6 +80,4 @@ const Contact = () => {
       </div>
     </Layout>
   );
-};
-
-export default Contact;
+}
