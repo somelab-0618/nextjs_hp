86acaeac4c84bbd3bb1d5f50737077976baa7303
RyosukeSomeya <rx.0.0618@gmail.com>
2022-05-29 00:23:52 +0900

    contact page修正

--- components/Layout.js
+++ components/Layout.js
@@ -36,6 +36,7 @@ export default function Layout({ children, title = 'HP by Next.js' }) {
       </main>
       <footer className='w-full h-12 flex justify-center items-center border-t'>
         <a
+          className='flex items-center'
           href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
           target='_blank'
           rel='noopener noreferrer'
